5fbbaea8d7fe96c2005449e4d93f3dbd4bc4221c
鲜青松 <1984564067@qq.com>
2025-03-06 18:50:33 +0800

    完善大部分功能

--- online-education-platform-common/src/main/java/com/xian/cotext/UserContext.java
+++ online-education-platform-common/src/main/java/com/xian/cotext/UserContext.java
@@ -6,7 +6,7 @@ public class UserContext {
     // 线程安全的ThreadLocal变量 阿萨·
     private static final ThreadLocal<Map<String, Object>> userContext = new ThreadLocal<>();
 
-    // 设置用户信息 到 Thread
+    // 设置用户信息 到 Thread as a Map
     public static void setUser(Map<String, Object> user) {
         userContext.set(user);
     }
